e356b901657a09445b245ad30cbdcce989d4e644
devbaggett <devabaggett@gmail.com>
2018-04-16 16:36:38 -0700

    added directives to display data in html

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  	selector: 'app-root',
+  	templateUrl: './app.component.html',
+  	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-  constructor(private _httpService: HttpService){}
+export class AppComponent implements OnInit{
+  	title = 'Restful Tasks API';
+  	tasks = [];
+  	constructor(private _httpService: HttpService){}
+  	ngOnInit(){
+      	this.getTasksFromService();
+    }
+  	getTasksFromService(){
+    	let observable = this._httpService.getTasks();
+    	observable.subscribe(data => {
+    		console.log("Got our tasks!", data);
+    		this.tasks = data['tasks'];
+    		console.log(this.tasks);
+    	});
+	}
+	getTaskFromService(id){
+		if (id){
+			let tempObservable = this._httpService.getTask(id);
+			tempObservable.subscribe(data => {
+				console.log("Got our task!", data);
+			})
+		}
+	}
+
+
 }
+
+
--- AngularApp/src/app/http.service.ts
+++ AngularApp/src/app/http.service.ts
@@ -5,10 +5,24 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 export class HttpService {
 	constructor(private _http: HttpClient) {
-		this.getTasks();
+		// this.getTasks();
+		// this.getTask();
 	}
 	getTasks(){
-		let tempObservable = this._http.get('/tasks');
-		tempObservable.subscribe(data => console.log('Got our tasks!', data));
+		// let tempObservable = this._http.get('/tasks');
+		// tempObservable.subscribe(data => console.log('Got our tasks!', data));
+		return this._http.get('tasks');
+	}
+	getTask(id){
+		return this._http.get('/retrieve/' + id);
+	}
+	createTask(task){
+		return this._http.post('/create', task);
+	}
+	updateTask(id, updatedTask){
+		return this._http.put('/update/' + id, updatedTask);
+	}
+	deleteTask(id){
+		return this._http.delete('/delete/' + id);
 	}
 }
